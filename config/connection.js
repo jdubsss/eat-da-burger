f9bfa800977a3a7653a49afd372c3ecee4ed2b27
jweber9 <jweber@oldcastlebe.com>
2017-09-30 20:50:51 -0600

    updated connection file

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,25 @@
 //export conenction to nbe used in orm file
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'burgers_db'
-})
+var connection;
+
+
 connection.connect(function(err) {
 	if (err) 
 		throw err;
 	console.log("Connected as id: " + connection.threadid);
 
 });
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: 'h40lg7qyub2umdvb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		user: 'qg19gi9uh7s7xxb9',
+		password: 'kuteprego6v6rq5e',
+		database: 'burgers_db'
+	})
+}
 
 module.exports = connection;
