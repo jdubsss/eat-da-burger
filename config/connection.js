ec751b8c8606532c85b0b85a116de90882da1190
jweber9 <jweber@oldcastlebe.com>
2017-09-30 21:26:06 -0600

    updated database name based on jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,21 @@
 //connect to mysql database and export to the orm file
 //export conenction to nbe used in orm file
 
-var mysql = require('mysql');
-var connection;
+var mysql = require("mysql");
 
-
-connection.connect(function(err) {
-	if (err) 
-		throw err;
-	console.log("Connected as id: " + connection.threadid);
-
-});
-
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-	connection = mysql.createConnection({
+var connection = mysql.createConnection({
 		host: 'h40lg7qyub2umdvb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
 		user: 'qg19gi9uh7s7xxb9',
 		password: 'kuteprego6v6rq5e',
-		database: 'burgers_db'
-	})
-}
+		database: 'm2zh4a0bvu1lflnf'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 module.exports = connection;
